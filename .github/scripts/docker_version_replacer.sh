f893d4fb2cb7f07abd86595e45d4a1003a326706
Allan Pedroni <98485166+abpedroni@users.noreply.github.com>
2024-01-09 17:34:27 -0300

    teste

--- .github/scripts/docker_version_replacer.sh
+++ .github/scripts/docker_version_replacer.sh
@@ -21,6 +21,30 @@ print_used_params()
     echo "------------------------------------------"
 }
 
+print_comment_header() 
+{
+    echo "|file|Current Versions|New Version|" >> dockerversion_replacer_out.md
+    echo "|---|---|---|" >> dockerversion_replacer_out.md   
+}
+
+add_file_to_comment() 
+{
+    file="$1"
+    current_versions="$2"
+    new_version="$3"
+
+    if [ -z "$file" ]; then
+        file="N/A"
+    fi
+
+    if ( [ -z "$current_versions" ] || [ "$current_versions" = "N/A" ] ); then
+        current_versions="N/A"
+        new_version="-"
+    fi
+    
+    echo "|$file|$current_versions|$new_version|" >> dockerversion_replacer_out.md
+}
+
 to_lower() {
     echo "$1" | awk '{print tolower($0)}' 
 }
@@ -29,34 +53,50 @@ commit_files() {
 
     if [ $(git status --porcelain | wc -l) -gt 0 ];
     then 
-        echo  "------------------------------------------"
-        echo "Commit files updated."
-        git config user.name "github-actions"
-        git config user.email "github-actions@users.noreply.github.com"
-        git add .
-        git commit -m "chore(deps): update $new_docker_version"
-        git push
+        # echo "------------------------------------------"
+        #echo "Commit files updated."
+         echo "LINE OF git push were disabled."
+        # pwd
+        # git branch --show-current
+        # git status
+
+        # git config user.name "github-actions"
+        # git config user.email "github-actions@users.noreply.github.com"
+        # git add .
+        # git commit -m "chore(deps): update $new_docker_version"
+        # git push
     fi
 }
 
-[ $1 ] || { echo -e "\e[31mERROR:\e[0m Invalid environment label (ex: dev, qa, prod)." >&2; usage; exit 1; }
-[ $2 ] || { echo -e "\e[31mERROR:\e[0mInvalid docker registry (ex: acrapplications.azurecr.io)." >&2; usage; exit 1; }
-[ $3 ] || { echo -e "\e[31mERROR:\e[0mNamespace used in the docker image version (ex: common)." >&2; usage; exit 1; }
-[ $4 ] || { echo -e "\e[31mERROR:\e[0mThe name of component. (ex: eventflowwebapi, liveagentmanagerwebapi)." >&2; usage; exit 1; }
-[ $5 ] || { echo -e "\e[31mERROR:\e[0mThe new docker image version. (ex: master.a1c905b.7290957852)." >&2; usage; exit 1; }
-#[ $6 ] || { echo -e "\e[31mERROR:\e[0mThe base or existence docker image version. (ex: master.a1c905b.7290957852)." >&2; usage; exit 1; }
+[ $1 ] || { echo "\e[31mERROR:\e[0m Invalid environment label (ex: dev, qa, prod)." >&2; usage; exit 1; }
+[ $2 ] || { echo "\e[31mERROR:\e[0mInvalid docker registry (ex: acrapplications.azurecr.io)." >&2; usage; exit 1; }
+[ $3 ] || { echo "\e[31mERROR:\e[0mNamespace used in the docker image version (ex: common)." >&2; usage; exit 1; }
+[ $4 ] || { echo "\e[31mERROR:\e[0mThe name of component. (ex: liveagentmanagerwebapi)." >&2; usage; exit 1; }
+[ $5 ] || { echo "\e[31mERROR:\e[0mThe new docker image version. (ex: master.a1c905b.7290957852)." >&2; usage; exit 1; }
+#[ $6 ] || { echo "\e[31mERROR:\e[0mThe base or existence docker image version. (ex: master.a1c905b.7290957852)." >&2; usage; exit 1; }
 
+print_comment_header
 print_used_params $1 $2 $3 $4 $5 $6
 
-environment_label="$1" #$1
+# environment_label="$1" 
+# cluster_aks="aks-sophie-$environment_label"
+# docker_registry="$2" 
+# namespace="$3" 
+# component="$4" 
+# new_docker_version="$5" 
+# base_docker_version="$6" 
+
+environment_label="dev"  #$1
 cluster_aks="aks-sophie-$environment_label"
-docker_registry="$2" #"acrapplications.azurecr.io"
-namespace="$3" #common
-component="$4" #eventflowwebapi
-new_docker_version="$5" #NEW_DOCKER_VERSION
-base_docker_version="$6"
+docker_registry="acrapplications.azurecr.io" #$2
+namespace="common" #$3
+component="eventflowwebapi" #$4
+new_docker_version="master.DEV.$RANDOM" #$5
+base_docker_version="" # "master.DEV" #$6
 
 echo "Deloyment environment: '$cluster_aks'."
+
+found_any_env=false
 
 for i in $(find . -type f \( -name "*.yaml" -o -name "*.yml" \));
 do
@@ -70,6 +110,7 @@ do
 
             if [ $(grep -e ":$base_docker_version\"" "$i" | wc -l) -gt 0 ]; 
             then 
+                found_any_env=true
                 echo "  * First attempt! \e[32m> FOUND the pattern $base_docker_version\"\e[0m";
                 
                 grep -e ":$base_docker_version\"" "$i" | awk '{print "  * Docker image found:", $2}' 
@@ -77,6 +118,7 @@ do
                 sed -i -E "s|(:)($base_docker_version)(\")|\1$new_docker_version\"|g" "$i"
                 echo "------------------------------------------"
                 ##sed -i $'s/$/\r/' "$i" #convert Unix to DOS/Windows format
+                cat "$i"
                 commit_files
                 continue;
             else
@@ -88,27 +130,32 @@ do
 
         if [ $(grep -e "\"$docker_registry\/$namespace\/$component:.*\"" "$i" | wc -l) -gt 0 ]; 
         then  
+            found_any_env=true
             echo "  * Second attempt! \e[32m> FOUND the pattern \"$docker_registry/$namespace/$component:.*\"\e[0m"; 
             grep -e "\"$docker_registry\/$namespace\/$component:.*\"" "$i" | awk '{print "  * Docker image found:", $2}' 
             echo "  * New docker version: $new_docker_version"
             sed -i -E "s|(\"$docker_registry\/$namespace\/$component:)(..*)\"|\1$new_docker_version\"|g" "$i"
             echo "------------------------------------------"
             ##sed -i $'s/$/\r/' "$i" #convert Unix to DOS/Windows format
+            cat "$i"
         else
             echo "  * Second attempt! \e[35m> NOT FOUND\e[0m"; 
 
             if [ $(grep -e "\/$component:.*\"" "$i" | wc -l) -gt 0 ]; 
-            then  
+            then 
+                found_any_env=true 
                 echo "  * Third attempt! \e[32m> FOUND the pattern /$component:.*\"\e[0m"; 
                 egrep -e "(\/$component:)(..*)(\")" "$i" | awk '{print "  * Docker image found:", $2}'
                 echo "  * New docker version: $new_docker_version"
                 sed -i -E "s|(\/$component:)(..*)(\")|\1$new_docker_version\3|g" "$i"
                 echo "------------------------------------------"
                 ##sed -i $'s/$/\r/' "$i" #convert Unix to DOS/Windows format
+                cat "$i"
             else
                 echo "  * Third attempt! \e[35m> NOT FOUND\e[0m"; 
                 if ([ $(egrep -e "repository: $docker_registry\/$namespace\/$component" "$i" | wc -l) -gt 0 ] && [ $(grep -e "tag:\s\".*\"" "$i" | wc -l) -gt 0 ]); 
                 then
+                    found_any_env=true
                     echo "Forth attempt! \e[32m> FOUND the pattern tag:\s\".*\"\e[0m"; 
                     #tag starting with " and ending with "
                     grep -e "tag:\s\".*\"" "$i" | awk '{print "  * Docker image found:", $2}' 
@@ -121,6 +168,7 @@ do
                     echo "  * New docker version: $new_docker_version"
                     echo "------------------------------------------"
                     ##sed -i $'s/$/\r/' "$i" #convert Unix to DOS/Windows format
+                    cat "$i"
                 else
                     echo "  * \e[31mResult: We didn't find any reference of the image $new_docker_version !!!\e[0m"; 
                     echo "------------------------------------------"
@@ -128,7 +176,7 @@ do
                 
             fi;
         fi;
-
+        
         commit_files
 
     else 
@@ -139,3 +187,7 @@ do
     fi;
 
 done;
+
+if [ "$found_any_env" = false ]; then
+    echo "\e[31mResult: We didn't find any reference of the image $new_docker_version in the choosed environment $environment_label or the environment doesn't exists !!!\e[0m"; 
+fi;
